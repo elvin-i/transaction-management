24d45587e3ec5e3697d14ebcd8b71710d81c273e
zhanglei <18001268330@163.com>
2026-02-09 06:24:44 +0800

    fix

--- transaction-management-frontend/src/components/GlobalHeader/RightContent.vue
+++ transaction-management-frontend/src/components/GlobalHeader/RightContent.vue
@@ -1,6 +1,12 @@
 <template>
   <div :class="wrpCls">
     <avatar-dropdown :menu="showMenu" :current-user="currentUser" :class="prefixCls" />
+    <a-tooltip v-if="showBackToNav" title="返回导航">
+      <span :class="prefixCls" @click="goNav">
+        <a-icon type="appstore" />
+        <span class="back-nav-label">返回导航</span>
+      </span>
+    </a-tooltip>
     <select-lang :class="prefixCls" />
   </div>
 </template>
@@ -46,12 +52,28 @@ export default {
         [`ant-pro-global-header-index-${(this.isMobile || !this.topMenu) ? 'light' : this.theme}`]: true
       }
     },
+    entry () {
+      const pathname = typeof window !== 'undefined' ? window.location.pathname : ''
+      return pathname.indexOf('nav') > -1 ? 'nav' : (pathname.indexOf('admin') > -1 ? 'admin' : (pathname.indexOf('ops') > -1 ? 'ops' : 'index'))
+    },
+    base () {
+      const pub = process.env.PUBLIC_PATH || '/'
+      return pub.endsWith('/') ? pub.slice(0, -1) : pub
+    },
+    showBackToNav () {
+      return this.entry === 'index' || this.entry === 'admin' || this.entry === 'ops'
+    },
     userInfo () {
       return this.$store.getters.userInfo
     }
   },
   created () {
     this.currentUser = this.userInfo
+  },
+  methods: {
+    goNav () {
+      window.location.href = `${this.base}/nav.html#/home`
+    }
   }
 }
 </script>
